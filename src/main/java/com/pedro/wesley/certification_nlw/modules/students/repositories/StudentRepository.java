55170895beb33fe79f71545580c7d8bb976f4b4a
Pedrowesley <pedrodev101@gmail.com>
2024-02-10 20:09:43 -0300

    feat(StudentRepository): Add StudentRepository interface
    - Add StudentRepository interface for handling database operations related to students.
    - This interface extends JpaRepository and provides methods for querying Student entities.
    - This will allow for easy and efficient interaction with the database for operations on students.

---
+++ src/main/java/com/pedro/wesley/certification_nlw/modules/students/repositories/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.pedro.wesley.certification_nlw.modules.students.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pedro.wesley.certification_nlw.modules.students.entities.StudentEntity;
+
+public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
+    Optional<StudentEntity> findByEmail(String email);
+
+}
