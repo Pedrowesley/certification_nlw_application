e6f08c104aabc6117e5d06a5bbc4ed7a0a4d0745
Pedrowesley <pedrodev101@gmail.com>
2024-02-10 20:12:48 -0300

    feat(StudentController): Add method for handling student certification answers
    - Add certificationAnswer method in StudentController class.
    - This method uses the StudentCertificationAnswersUseCase to process the student's answers to certification questions.
    - This addition will allow the application to handle HTTP POST requests at the "/certification/answer" endpoint, improving the functionality of the student certification process.

--- src/main/java/com/pedro/wesley/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/com/pedro/wesley/certification_nlw/modules/students/controllers/StudentController.java
@@ -2,7 +2,9 @@ package com.pedro.wesley.certification_nlw.modules.students.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pedro.wesley.certification_nlw.modules.students.dto.StudentCertificationAnswersDTO;
 import com.pedro.wesley.certification_nlw.modules.students.dto.VerififyHasCertificationDTO;
+import com.pedro.wesley.certification_nlw.modules.students.useCases.StudentCertificationAnswersUseCase;
 import com.pedro.wesley.certification_nlw.modules.students.useCases.VerifyHasCertificationUseCase;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,9 @@ public class StudentController {
     @Autowired
     private VerifyHasCertificationUseCase verifyHasCertificationUseCase;
 
+    @Autowired
+    private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
+
     @PostMapping("verififyIfHasCertification")
     public String verififyIfHasCertification(@RequestBody VerififyHasCertificationDTO verififyHasCertificationDTO) {
         var result = verifyHasCertificationUseCase.execute(verififyHasCertificationDTO);
@@ -24,5 +29,10 @@ public class StudentController {
             return "Usuário possui a certificação";
         }
         return "Usuário não possui a certificação";
+    }
+
+    @PostMapping("/certification/answer")
+    public StudentCertificationAnswersDTO certificationAnswer(@RequestBody StudentCertificationAnswersDTO dto) {
+        return studentCertificationAnswersUseCase.execute(dto);
     }
 }
