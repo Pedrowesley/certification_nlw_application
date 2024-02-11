12ae8e1bdf94a54db1265e68f1ff74bc54bcd6fd
Pedrowesley <pedrodev101@gmail.com>
2024-02-10 23:25:45 -0300

    feat(Top10RankingUseCases): Add use case for top 10 rankings
    - Add Top10RankingUseCases class which fetches the top 10 students based on their certification grades.
    - This class uses the CertificationStudentRepository to retrieve the data.
    - This addition will provide the necessary logic to retrieve the top 10 students, enhancing the application's functionality.

---
+++ src/main/java/com/pedro/wesley/certification_nlw/modules/certifications/useCases/Top10RankingUseCases.java
@@ -0,0 +1,19 @@
+package com.pedro.wesley.certification_nlw.modules.certifications.useCases;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedro.wesley.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import com.pedro.wesley.certification_nlw.modules.students.repositories.CertificationStudentRepository;
+
+@Service
+public class Top10RankingUseCases {
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
+    public List<CertificationStudentEntity> execute() {
+        return this.certificationStudentRepository.findTop10ByOrderGradeDesc();
+    }
+}
