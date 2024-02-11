5d2e74880d8912261427d9375aedce78f88602f4
Pedrowesley <pedrodev101@gmail.com>
2024-02-10 21:11:40 -0300

    feat(CertificationStudentEntity): Add annotations and cascade type
    - Add necessary JPA annotations to CertificationStudentEntity class.
    - Set cascade type for the relationship between CertificationStudentEntity and related entities.
    - These changes will ensure proper mapping of the entity to the database and manage the persistence lifecycle of related entities.

--- src/main/java/com/pedro/wesley/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/pedro/wesley/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -3,6 +3,9 @@ package com.pedro.wesley.certification_nlw.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,10 +15,12 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "certifications")
@@ -37,8 +42,9 @@ public class CertificationStudentEntity {
     @JoinColumn(name = "student_id", insertable = false, updatable = false)
     private StudentEntity studentEntity;
 
-    @OneToMany()
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "answers_certification_id", insertable = false, updatable = false)
+    @JsonManagedReference
     List<AnswersCertificationsEntity> answersCertificationsEntity;
 
 }
